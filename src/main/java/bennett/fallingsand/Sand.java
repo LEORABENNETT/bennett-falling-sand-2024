32afe2f29e795f1fc969ae765c06fae7fb9ec390
leorabennett <lbennett9@student.touro.edu>
2024-03-06 12:30:30 -0500

    falling sand simulation homework

--- src/main/java/bennett/fallingsand/Sand.java
+++ src/main/java/bennett/fallingsand/Sand.java
@@ -30,7 +30,15 @@ public class Sand {
     }
 
     public void fall() {
-        // moves all sand down one square
-
+        for (int x = 0; x < field[0].length; x++) {
+            for (int y = field.length - 2; y >= 0; y--) {
+                if (field[y][x] == 1) {
+                    if (field[y + 1][x] == 0) {
+                        field[y + 1][x] = 1;
+                        field[y][x] = 0;
+                    }
+                }
+            }
+        }
     }
 }
